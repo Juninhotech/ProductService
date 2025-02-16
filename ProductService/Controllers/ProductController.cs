bf402f2ab1dc3b1b22d98f61ddfe01df1253303c
Juninho <juninhopeter1@gmail.com>
2025-02-16 09:37:52 +0100

    Add project files.

---
+++ ProductService/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductService.Data;
+using ProductService.Models;
+
+namespace ProductService.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDbContext _context;
+        private readonly HttpClient _httpClient;
+
+        public ProductController(IHttpClientFactory httpClientFactory, ProductDbContext context)
+        {
+            _httpClient = httpClientFactory.CreateClient();
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrder(int userId, string productName)
+        {
+            var user = await _httpClient.GetFromJsonAsync<User>($"https://localhost:7272/api/users/{userId}");
+            if (user == null) return BadRequest("Invalid user");
+
+            var product = new Product { UserId = userId, ProductName = productName };
+            _context.Products.Add(product   );
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+    }
+}
---
+++ ProductService/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductService.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string ProductName { get; set; }
+    }
+}
